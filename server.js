174ba22daf1bf54db795c0e884d669b4ed8484ee
Breeze1203 <99305961+Breeze1203@users.noreply.github.com>
2025-10-31 09:48:03 +0800

    Initialize Express server with versioning

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// 我们用一个环境变量来模拟版本
+const VERSION = process.env.APP_VERSION || "v1.0 (Default)";
+
+app.get('/', (req, res) => {
+  console.log(`[${VERSION}] Request received!`);
+  res.send(`Hello from ${VERSION}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} with ${VERSION}`);
+});
